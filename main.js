31e7adfe2c168bc0b325025150ce2b2c040f9fd0
Mingsterism <ming.k.@hotmail.com>
2018-09-24 23:49:39 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,79 @@
+var XLSX = require('xlsx');
+var aStore = require('app-store-scraper');
+var gStore = require('google-play-scraper');
+
+var config = require("./config.json")       // config file to select which column you want to get the values from
+var workbook = XLSX.readFile('./Apps-and-etools-library.xlsx'); // change this if needed
+
+var first_sheet_name = workbook.SheetNames[0];
+
+function main (arrayValue) {
+    console.log(arrayValue)
+    var column = config[arrayValue].column
+    var startrow = config[arrayValue].startrow
+    var endrows = config[arrayValue].endrow 
+    var all_addresses = []
+    for (var x = startrow; x <= endrows; x++ ) {
+        all_addresses.push(column+x)
+    }
+    /* Get worksheet */
+    var worksheet = workbook.Sheets[first_sheet_name];
+
+    /* Find desired cell */
+    var results = []
+    for (var cell in all_addresses) {
+        var value = worksheet[all_addresses[cell]]
+        if (value) {  
+            results.push({ cellValue: value.v, cellAddress: all_addresses[cell]})
+        } else {
+            console.log("NOOOOOOOOOOOOOOOO error undefined >>> ", value)
+        }
+    }
+    return results
+}
+
+// querying the app store  
+function getAppleData() {
+    var appleIds = main(0)
+    for (var appleObject in appleIds) {
+        var aId = appleIds[appleObject].cellValue
+        aStore.app({id: aId}).then((data) => {
+            console.log({
+                dataId: data.id, 
+                price: data.price,
+                updated: data.updated, 
+                version: data.version, 
+                developer: data.developer, 
+                developerUrl: data.developerUrl
+            }) 
+            console.log("")
+        }).catch( (err) => {
+            // console.log("Tehre was error: ", err)
+        });
+    }
+}
+
+// querying the google store 
+function getGoogleData () {
+    var googleIds = main(1)
+    for (var googleObject in googleIds) {
+        var gId = googleIds[googleObject].cellValue
+        gStore.app({appId: gId}).then((data) => {
+            console.log(data)
+            console.log({
+                dataId: data.appId, 
+                price: data.price,
+                versionText: data.androidVersionText, 
+                developer: data.developer, 
+                developerWebsite: data.developerWebsite
+            }) 
+            console.log("")
+        }).catch( (err) => {
+        });
+    }
+}
+
+getGoogleData()
+getAppleData()
+
+ 
